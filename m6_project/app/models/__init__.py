30a3d38b9a898dd34498d0b4d6c65413f2240286
kondrusev33ch <kondrusev33ch@gmail.com>
2026-02-07 19:55:00 +0300

    orders

--- m6_project/app/models/__init__.py
+++ m6_project/app/models/__init__.py
@@ -1,7 +1,8 @@
 from .categories import Category
 from .products import Product
+from .orders import Order, OrderItem
 from .users import User
 from .reviews import Review
 from .cart_items import CartItem
 
-__all__ = ["Category", "CartItem", "Product", "User", "Review"]
+__all__ = ["Category", "CartItem", "Product", "User", "Review", "Order", "OrderItem"]
--- m6_project/app/models/products.py
+++ m6_project/app/models/products.py
@@ -40,6 +40,7 @@ class Product(Base):
     seller: Mapped["User"] = relationship("User", back_populates="products")
     reviews: Mapped[list["Review"]] = relationship("Review", back_populates="product")
     cart_items: Mapped[list["CartItem"]] = relationship("CartItem", back_populates="product", cascade="all, delete-orphan")
+    order_items: Mapped[list["OrderItem"]] = relationship("OrderItem", back_populates="product")
 
     __table_args__ = (
         Index("ix_products_tsv_gin", "tsv", postgresql_using="gin"),
