adace8f97225b0d60a5ba5799bc33db95fafa058
kondrusev33ch <kondrusev33ch@gmail.com>
2026-02-07 19:25:00 +0300

    cart system

--- m6_project/app/models/products.py
+++ m6_project/app/models/products.py
@@ -39,6 +39,7 @@ class Product(Base):
     category: Mapped["Category"] = relationship("Category", back_populates="products")
     seller: Mapped["User"] = relationship("User", back_populates="products")
     reviews: Mapped[list["Review"]] = relationship("Review", back_populates="product")
+    cart_items: Mapped[list["CartItem"]] = relationship("CartItem", back_populates="product", cascade="all, delete-orphan")
 
     __table_args__ = (
         Index("ix_products_tsv_gin", "tsv", postgresql_using="gin"),
